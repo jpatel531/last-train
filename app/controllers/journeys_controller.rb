f4ef3db33ac11a2cca57305ab216f65eac08ef45
jpatel531 <jamie@notespublication.com>
2014-09-06 21:46:17 +0100

    Gem now fetches correct date

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -9,6 +9,7 @@ class JourneysController < ApplicationController
 		client = TFLJourneyPlanner::Client.new(app_id: Rails.application.secrets.tfl_id, app_key: Rails.application.secrets.tfl_key)
 		date = (Time.now < Time.parse('5am')) ? nil : (Time.now + (60 * 60 * 24)).strftime("%Y%m%d")
 		@journeys = client.get_journeys from: params[:from], to: params[:to], time: "0300", time_is: "Arriving", date: date
+		
 		render "create.json.jbuilder"
 	end
 
